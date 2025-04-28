9bb0e015a682480ccf27f8307eec99472a3db5dd
RubioDiamant <mael.chalot@gmail.com>
2025-04-28 11:18:57 +0200

    Implementation list dynamique Algo3

--- Moodle RSA/phase3.c
+++ Moodle RSA/phase3.c
@@ -7,6 +7,82 @@
 #define TAILLE_MAX_NOM_FICHIER 30
 #define TAILLE_MAX_COMMANDE 100
 
+//------------------------------------Structure dynamique pour stocker les clés---------------------------------------
+typedef struct{
+    rsaKey_t public;
+    rsaKey_t private;
+    int id;
+}keyIdentifier;
+
+typedef struct s_node{
+    struct s_node* next;
+    struct s_node* previous;
+    keyIdentifier* keyStruct;
+}node;
+
+
+typedef struct{
+    node *sentinel;
+    int size;
+}keysDynamicList;
+
+
+
+keysDynamicList* list_create(void) {
+	keysDynamicList* l = malloc(sizeof(keysDynamicList));
+	l->size = 0;
+	l->sentinel = malloc(sizeof(node));
+	l->sentinel->next = l->sentinel;
+	l->sentinel->previous = l->sentinel;
+	return l;
+}
+
+void list_delete(keysDynamicList** l) {
+	node* element;
+	while((*l)->sentinel->next != (*l)->sentinel){
+		element = (*l)->sentinel->next;
+		(*l)->sentinel->next = element->next;
+        free(element->keyStruct);
+		free(element);
+		(*l)->size -= 1;
+	}
+	free(((*l)->sentinel)); //ou element = (*l)-> sentinel et free(element)
+	free(*l);
+	*l=NULL;
+}
+
+keyIdentifier* createNewKeys(rsaKey_t public, rsaKey_t private, int id){
+    keyIdentifier *new = malloc(sizeof(keyIdentifier));
+    new->id = id;
+    new ->private = private;
+    new->public = public;
+    return new;
+}
+
+keysDynamicList* list_push_back(keysDynamicList* l, keyIdentifier* keys) {
+	node* newElement = malloc(sizeof(node));
+	newElement->keyStruct = keys;
+	l->sentinel->previous->next = newElement;
+	newElement->next = l->sentinel;
+	newElement->previous = l->sentinel->previous;
+	l->sentinel->previous = newElement;
+	l->size += 1;
+	return l;
+}
+
+keysDynamicList* list_map(keysDynamicList* l) {
+	node* element = l->sentinel->next;
+	while (element != l->sentinel)
+	{
+		printf("Paire n° %d : ", element->keyStruct->id);
+        affichageClefs(&(element->keyStruct->public), &(element->keyStruct->private));
+        element = element->next;
+	}
+	return l;
+}
+
+//--------------------------------------------------------------------------------------------------------------------
+
 void clearBuffer()
 {
     // Vidage de la memoire tampon pour clear stdin
@@ -15,13 +91,15 @@ void clearBuffer()
         ;
 }
 
-void genererPairCle()
+void genererPairCle(keysDynamicList* keyList,int id)
 {
     printf("Generation des clés publique et privee...\n");
     rsaKey_t publicKey, privateKey;
     genKeysRabin(&publicKey, &privateKey, MAX_PRIME);
     printf("La paire de cle a ete generee :\n");
     affichageClefs(&publicKey, &privateKey);
+    list_push_back(keyList, createNewKeys(publicKey, privateKey,id));
+    list_map(keyList);
 }
 
 void convertBinaryTob64(char* in, char *out)
@@ -62,6 +140,7 @@ void convertb64ToBinary(char *in, char *out)
 
 int main()
 {
+    keysDynamicList* mainKeyList = list_create();
     char commande[TAILLE_MAX_COMMANDE];
     char choix[50];
     char quitter;
@@ -90,7 +169,11 @@ int main()
             printf("https://moodle.univ-tlse3.fr/pluginfile.php/1126789/mod_folder/content/0/interpre%%CC%%80te.pdf?forcedownload=1\n");
         }
         else if(strcmp(choix, "newkeys") == 0){
-            genererPairCle();
+            char idChar[10];
+            int id;
+            sscanf(commande, "%s %s", choix, idChar);
+            id = atoi(idChar);
+            genererPairCle(mainKeyList, id);
         }
         else if(strcmp(choix, "bin-2b64") == 0){
             char in[TAILLE_MAX_NOM_FICHIER];
@@ -109,4 +192,6 @@ int main()
         }
 
     } while (quitter != 'y');
+    list_delete(&mainKeyList);
+    printf("Exit success\n");
 }
